d03451a9acba0c32c75c1c4359a1c2783400cfa6
Pichsereyvattana Chan <pchan@ithaca.edu>
2023-01-01 17:28:15 -0500

    Added more tests

--- tests/file_test.go
+++ tests/file_test.go
@@ -50,3 +50,31 @@ func TestDownladFileNotExist(t *testing.T) {
 	assert.Equal(t, "{\"message\":\"Could not get download link\",\"code\":404,\"error\":\"%!s(\\u003cnil\\u003e)\"}\n", rec.Body.String())
 	assert.Equal(t, 404, c.Response().Status)
 }
+
+func TestHideFile(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath("/hide/:fileUuid")
+	c.SetParamNames("fileUuid")
+	c.SetParamValues("1724a4ce-3e1b-4562-abb6-bb166a746414")
+	err := handlers.HideFile(c)
+	assert.NoError(t, err)
+	assert.Equal(t, "{\"message\":\"File is hidden\",\"code\":200,\"error\":\"none\"}\n", rec.Body.String())
+	assert.Equal(t, 200, c.Response().Status)
+}
+
+func TestUnhideFile(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath("/unhide/:fileUuid")
+	c.SetParamNames("fileUuid")
+	c.SetParamValues("1724a4ce-3e1b-4562-abb6-bb166a746414")
+	err := handlers.UnhideFile(c)
+	assert.NoError(t, err)
+	assert.Equal(t, "{\"message\":\"File is unhidden\",\"code\":200,\"error\":\"none\"}\n", rec.Body.String())
+	assert.Equal(t, 200, c.Response().Status)
+}
